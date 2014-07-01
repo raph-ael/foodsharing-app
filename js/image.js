60f47b6ffdd9ed596ca508ade06181547bd33b27
Raphael <raphi@waldorfweb.net>
2014-07-01 11:37:01 +0200

    auf pg version 3.4 gewechselt

---
+++ js/image.js
@@ -0,0 +1,123 @@
+	var pictureSource;   // picture source
+    var destinationType; // sets the format of returned value 
+
+    // Wait for Cordova to connect with the device
+    //
+    document.addEventListener("deviceready",onDeviceReady,false);
+
+    // Cordova is ready to be used!
+    //
+    function onDeviceReady() {
+        pictureSource=navigator.camera.PictureSourceType;
+        destinationType=navigator.camera.DestinationType;
+    }
+
+    // Called when a photo is successfully retrieved
+    //
+    function onPhotoDataSuccess(imageData) {
+      // Uncomment to view the base64 encoded image data
+      // console.log(imageData);
+
+      // Get image handle
+      //
+      var smallImage = document.getElementById('smallImage');
+
+      // Unhide image elements
+      //
+      smallImage.style.display = 'block';
+
+      // Show the captured photo
+      // The inline CSS rules are used to resize the image
+      //
+      smallImage.src = "data:image/jpeg;base64," + imageData;
+      
+      //uploadPhoto();
+    }
+
+    // Called when a photo is successfully retrieved
+    //
+    function onPhotoURISuccess(imageURI) {
+      // Uncomment to view the image file URI 
+      // console.log(imageURI);
+
+      // Get image handle
+      //
+      var largeImage = document.getElementById('largeImage');
+
+      // Unhide image elements
+      //
+      largeImage.style.display = 'block';
+
+      // Show the captured photo
+      // The inline CSS rules are used to resize the image
+      //
+      largeImage.src = imageURI;
+      
+      uploadPhoto(imageURI);
+    }
+
+    // A button will call this function
+    //
+    function capturePhoto() {
+      // Take picture using device camera and retrieve image as base64-encoded string
+      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 65,targetWidth: 450, targetHeight: 450,
+
+        destinationType: destinationType.FILE_URI });
+    }
+
+    // A button will call this function
+    //
+    function capturePhotoEdit() {
+      // Take picture using device camera, allow edit, and retrieve image as base64-encoded string  
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
+        destinationType: destinationType.DATA_URL });
+    }
+
+    // A button will call this function
+    //
+    function getPhoto(source) {
+      // Retrieve image file location from specified source
+      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, 
+        destinationType: destinationType.FILE_URI,
+        sourceType: source });
+    }
+
+    // Called if something bad happens.
+    // 
+    function onFail(message) {
+      //alert('Failed because: ' + message);
+    }
+
+	function uploadPhoto(imageURI)
+	{
+		loader.miniShow();
+		var options = new FileUploadOptions();
+		options.fileKey="file";
+		options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+		options.mimeType="image/jpeg";
+		var params = new Object();
+		params.value1 = "test";
+		params.value2 = "param";
+		options.params = params;
+		options.chunkedMode = false;
+		var ft = new FileTransfer();
+		ft.upload(imageURI, 'http://' + c.domain + '/freiwillige/xhrapp.php?app=api&m=upload', win, fail, options);
+	}
+	function win(r) 
+	{
+		loader.miniHide();
+		if(r.response.length > 0)
+		{
+			$('#photo').val(r.response+'');
+			$('#image-preview').css('display','block');
+		}
+		else
+		{
+			msg.error("Das Foto konnte leider nicht hochgeladen werden, Bitter überprüfe Deine Internet-Verbindung!" );
+		}
+	}
+	function fail(error) 
+	{
+		loader.miniHide();
+		msg.error("Das Foto konnte leider nicht hochgeladen werden, Bitter überprüfe Deine Internet-Verbindung!" );
+	}  
